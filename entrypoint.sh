835d651a4e7a7d1a5521af4783f3e1ead2c7953e
Serhii5465 <serhiiartiukh5@gmail.com>
2024-01-20 19:55:52 +0200

    Added command for launch server

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 steamcmd +force_install_dir ${HOME}/${DIR_SERVER}/ +login anonymous +app_update 222860 validate +quit
+
+${DIR_SERVER}/srcds_run -game left4dead2 -port 27015 +map c1m1_hotel -norestart -console
