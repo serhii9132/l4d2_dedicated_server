b064a4792b5e2a45804bd414d63b2d2749656135
Serhii <serhiiartiukh5@gmail.com>
2025-06-25 16:47:25 +0300

    entrypoint.sh: Fix "Invalid platform" error of SteamCMD

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-steamcmd +force_install_dir ${PATH_SERVER} +login anonymous +app_update 222860 validate +quit
+steamcmd +force_install_dir ${PATH_SERVER} +login anonymous +@sSteamCmdForcePlatformType windows +app_update 222860 validate +quit
+steamcmd +force_install_dir ${PATH_SERVER} +login anonymous +@sSteamCmdForcePlatformType linux +app_update 222860 validate +quit
 
 ${PATH_SERVER}/srcds_run -game left4dead2 -port 27015 $@
